1fe598f7f96dc5db28d072a0b48d14dcb1b9ee4e
omkar nath <omkarnath1123@gmail.com>
2016-04-11 00:50:45 +0530

    Limited Player Health

---
+++ PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	// ... for values true or false ...
+	public bool alive;
+	[SerializeField]
+	private GameObject pickupPrefab;
+
+	// Use this for initialization
+	void Start () {
+		alive = true;
+	}
+	
+	void OnTriggerEnter(Collider other){
+		// ... comparing the collision with bird by enemy tag ...
+		// ... one more verification .. if the player will collide with bird with multiple times ...
+		if (other.CompareTag ("Enemy") && alive == true) {
+			alive = false;
+
+			// ... create the pick up particles ...
+			Instantiate(pickupPrefab, transform.position, Quaternion.identity);
+
+		}
+	}
+}
