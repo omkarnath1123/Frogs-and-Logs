7d113e186697b95740f5f0e0bd0adc9c74434fbe
omkar nath <omkarnath1123@gmail.com>
2016-04-11 00:48:27 +0530

    Fly Spawner

---
+++ FlySpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlySpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject flyPrefab;
+
+	[SerializeField]
+	private int totalFlyMinimum = 12;
+
+	private float spawnArea =25f;
+
+	public static int totalFlies;
+
+	// Use this for initialization
+	void Start () {
+		totalFlies = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		while (totalFlies < totalFlyMinimum){
+			totalFlies++;
+
+			// ... create a random position of fly ...
+			float positionX = Random.Range(-spawnArea, spawnArea);
+			float positionZ = Random.Range(-spawnArea, spawnArea);
+
+			Vector3 flyPosition = new Vector3(positionX, 2f, positionZ);
+
+			// ... and create a new fly ...
+			Instantiate(flyPrefab, flyPosition, Quaternion.identity);
+	}
+}
+}
