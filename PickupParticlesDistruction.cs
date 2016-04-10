9241857b66c47528a12ae905c0644f3ec5e07098
omkar nath <omkarnath1123@gmail.com>
2016-04-11 00:50:27 +0530

    Pick Up particles Distruction

---
+++ PickupParticlesDistruction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupParticlesDistruction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// ... destroy the pick up particles after 5 sec ...
+		Destroy (gameObject, 5f);
+	}
+	
+
+}
