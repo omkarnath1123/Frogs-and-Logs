da0b700ea8e4309bba6de6e645f0410b54f1889a
omkar nath <omkarnath1123@gmail.com>
2016-04-11 00:48:11 +0530

    Fly pickup Particles

---
+++ FlyPickup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyPickup : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject pickupPrefab;
+	
+	void OnTriggerEnter(Collider other) {
+		// if the "Collider" other is tagged with "Player"...
+		if (other.CompareTag ("Player")) {
+
+			// ... add the pickup particles ...
+			Instantiate(pickupPrefab, transform.position, Quaternion.identity);
+
+			//  ... decrement the total no. fly's ...
+			FlySpawner.totalFlies--;
+
+			// ... update the score ...
+			ScoreCounter.score++;
+
+			Destroy (gameObject);
+		}
+	}
+}
