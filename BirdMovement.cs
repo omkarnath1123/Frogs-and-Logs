72320c5f0ee2776ab2c4e0256716e2e75bc2d2f8
omkar nath <omkarnath1123@gmail.com>
2016-04-11 00:46:23 +0530

    Bird Movement

---
+++ BirdMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+
+	[SerializeField]
+	private Transform target;
+
+	private NavMeshAgent birdAgent;
+	private Animator birdAnimator;
+	[SerializeField]
+	private RandomSoundPlayer birdFootsteps;
+
+	// Use this for initialization
+	void Start () {
+	
+		birdAgent = GetComponent<NavMeshAgent> ();
+		birdAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// ... set to the bird destination to frog when every frames moves ...
+		birdAgent.SetDestination (target.position);
+		// ... measure the velocity of newmesh component ...
+		float speed = birdAgent.velocity.magnitude;
+		// ... pass the velocity to the animator component ...
+		birdAnimator.SetFloat ("Speed", speed);
+
+		if (speed > 0f) {
+			birdFootsteps.enabled = true;
+		} else {
+			birdFootsteps.enabled = false;
+		}
+	}
+}
